0e25ae70066891ca046c2309c1457c1c8ba1b0ad
Josef Heidler <heidlerjosef@gmail.com>
2019-03-02 20:34:02 +0100

    Diagnostika
    Přidána kompletní diagnostika, rework barev na primary, upravení footeru.

--- plochonozi/js/app.js
+++ plochonozi/js/app.js
@@ -6,6 +6,7 @@ function initComputer(){
     var $wrapper = $('#computer');
     var $button = $wrapper.find('[type="submit"]');
     var $result = $wrapper.find('[data-result]');
+    var $resultBox = $wrapper.find('[data-result-box]');
     $button.on('click', function(){
         var val1 = parseFloat($wrapper.find('#val1').val());
         var val2= parseFloat($wrapper.find('#val2').val());
@@ -13,30 +14,31 @@ function initComputer(){
 
         var group; //default
         if(result == 0) {
-            group = "0 nula je nula. prej tu ma byt nejaky info";
+            group = "Hodnota b nesmí být rovna 0.";
         }
         else if(inRange(result, 0.1, 25)) {
-            group = "Stupeň normálně klenuté nohy (0,1-25%)";
+            group = "Tato hodnota odpovídá stupni normálně klenuté nohy (0,1–25 %).";
         }
         else if(inRange(result, 25.1, 40)) {
-            group = "Stupeň normálně klenuté nohy (25,1-40%)";
+            group = "Tato hodnota odpovídá stupni normálně klenuté nohy (25,1–40 %).";
         }
         else if(inRange(result, 40.1, 45)) {
-            group = "Stupeň normálně klenuté nohy (40,1-45%)";
+            group = "Tato hodnota odpovídá stupni normálně klenuté nohy (40,1–45 %).";
         }
         else if(inRange(result, 45.1, 50)) {
-            group = "Stupeň ploché nohy (45,1%-50%)";
+            group = "Tato hodnota odpovídá stupni ploché nohy (45,1–50 %).";
         }
         else if(inRange(result, 50.1, 60)) {
-            group = "Stupeň ploché nohy (50,1%-60%)";
+            group = "Tato hodnota odpovídá stupni ploché nohy (50,1–60 %).";
         }
         else if(inRange(result, 60.1, 100)) {
-            group = "Stupeň ploché nohy (60,1%-100%)";
-        }else{
-            group = "neco se rozbilo. zadals hodnoty?"
+            group = "Tato hodnota odpovídá stupni ploché nohy (60,1–100 %).";
+        } else{
+            group = "Zadal jsi správně hodnoty a, b?"
         };
 
-        $result.text(result + " skupina: " + group);
+        $result.text(" Výsledek indexu nohy je " + result + ". " + group);
+        $resultBox.css("visibility","visible");
     })
 }
 
